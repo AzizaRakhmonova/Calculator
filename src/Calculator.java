426f99bce1cf3cd827a344de03f8570ba53be3cc
Aziza <azizulya@gmail.com>
2019-01-19 14:41:45 -0500

    fixed conflict

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,6 @@
 public class Calculator {
     public static double add (double num1, double num2) {
+        //this is a very useful method
         return num1 + num2;
         System.out.println(num1+num2)
 
